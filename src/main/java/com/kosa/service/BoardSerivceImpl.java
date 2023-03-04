4913757305d2648557fd75085051d4402f1df95a
lhyeonj120 <isk04387@gmail.com>
2023-03-05 00:04:13 +0900

    feat : Service 인터페이스의 구현체 생성

---
+++ src/main/java/com/kosa/service/BoardSerivceImpl.java
@@ -0,0 +1,46 @@
+package com.kosa.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.kosa.domain.BoardVO;
+import com.kosa.mapper.BoardMapper;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j;
+
+@Log4j
+@Service
+@AllArgsConstructor
+public class BoardSerivceImpl implements BoardService {
+
+	private BoardMapper mapper;
+
+	@Override
+	public BoardVO get(int b_id) {
+		return mapper.get(b_id);
+	}
+
+	@Override
+	public List<BoardVO> getList() {
+		return mapper.getList();
+	}
+
+	@Override
+	public void register(BoardVO board) {
+		mapper.createSelectKey(board);
+	}
+
+	@Override
+	public boolean modify(BoardVO board) {
+		return mapper.update(board) == 1 ? true : false;
+	}
+
+	@Override
+	public boolean remove(int b_id) {
+		return mapper.remove(b_id) == 1 ? true: false;
+	}
+
+}
