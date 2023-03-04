797854b1504ca6042529a1f75ca3ec28f05f9c3c
lhyeonj120 <isk04387@gmail.com>
2023-03-04 21:55:03 +0900

    test : 글 전체 read 테스트

---
+++ src/test/java/com/kosa/mapper/BoardMapperTests.java
@@ -0,0 +1,22 @@
+package com.kosa.mapper;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import lombok.extern.log4j.Log4j;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+@Log4j
+public class BoardMapperTests {
+	@Autowired
+	private BoardMapper mapper;
+	
+	@Test
+	public void getListTest() {
+		mapper.getList().forEach(board -> log.info(board));
+	}
+}
